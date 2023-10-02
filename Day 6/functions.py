d0bfa6501c0920ebefdcdc119ed7c9889e0019ec
peterston-e <peterfaretra77@gmail.com>
2023-10-02 21:42:19 +0100

    Day 6

--- Day 6/functions.py
+++ Day 6/functions.py
@@ -5,3 +5,54 @@ def my_function():
 
 my_function()
 
+# while true do something
+i=0
+while i < 5:
+    print("hello")
+    i += 1
+
+# while not at_goal():
+#     jump()
+
+#  for loops are good to iterate oveer something and to do something with each item. cant really do that with a while loop
+
+# while loops are more for doing something for ever untill a certain condition is met. need to avoid infinate loops unless you really want to. 
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+# ********************************
+# MAZE
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while not at_goal():
+#     if front_is_clear() and wall_on_right():
+#         move()
+#     elif right_is_clear():
+#         turn_right()
+#         move()
+#         while wall_on_right() and front_is_clear():
+#             move()
+#     else:
+#         if wall_in_front():
+#             turn_left()
+    
