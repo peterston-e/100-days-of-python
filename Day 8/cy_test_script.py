2f1ed642ee2b408735c491eef60da3db3a73d588
peterston-e <peterfaretra77@gmail.com>
2023-10-05 07:20:59 +0100

    cypher

---
+++ Day 8/cy_test_script.py
@@ -0,0 +1,26 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# for i in range(len(alphabet)):
+#     print(i)
+print((5 + 16) % 26) 
+print((5 + 17) % 26) 
+print((5 + 18) % 26) 
+print((5 + 19) % 26) 
+print((5 + 20) % 26) 
+print((5 + 21) % 26) 
+print((5 + 22) % 26) 
+
+print(len(alphabet))
+
+# The Modulus is the remainder of the euclidean division of one number by another. % is called the modulo operation.
+# For instance, 9 divided by 4 equals 2 but it remains 1. Here, 9 / 4 = 2 and 9 % 4 = 1.
+# In your example: 5 divided by 7 gives 0 but it remains 5 (5 % 7 == 5).
+
+# basically you will get the number youre putting in untill you get to the number youre dividing by
+
+string = "hello"
+
+# str_list = string.split()
+# print(str_list)
+
+# for i in str_list:
