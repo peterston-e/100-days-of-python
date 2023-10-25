3f605b1b50bf85421a2ceeb2183b4d4baaba2025
peterston-e <peterfaretra77@gmail.com>
2023-10-25 19:10:44 +0100

    data entry

---
+++ Day 14/higer_lower.py
@@ -0,0 +1,18 @@
+# Import random module or maybe just .chose()
+
+# print Askii art higher lower
+
+# save random person from the list to variable A
+# save random person from the list to variabke B
+
+# print(f"Compare A: {A_name}, a {A_description}, from {A_country}.")
+
+# print vs askii
+
+# print(f"Against B: {B_name}, a {B_description}, from {B_country}.")
+
+# create a compair function
+
+# create swap function for B to A
+
+# save new random person to B variable 
